a0e4d24ee366e2aeb423662eab380d619ee9b2b9
tk <83660733+tk-21@users.noreply.github.com>
2022-10-13 05:19:07 +0900

    OpinionController作成

---
+++ app/Http/Controllers/OpinionController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OpinionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    
+//    意見作成画面を表示
+    public function create()
+    {
+        $topic = new TopicModel;
+
+        $topic->id = get_param('id', null, false);
+
+        $fetchedTopic = TopicQuery::fetchById($topic);
+
+        // トピックが取れてこなかったら４０４ページへリダイレクト
+        if (!$fetchedTopic) {
+            redirect('404');
+            return;
+        }
+
+        // バリデーションに引っかかって登録に失敗した場合の処理
+        // セッションに保存しておいた値を取ってきて変数に格納する。セッション上のデータは削除する
+        // 必ずデータを取得した時点でデータを削除しておく必要がある。そうしないと他の記事を選択したときに出てきてしまう。
+        $opinion = OpinionModel::getSessionAndFlush();
+
+        if (empty($opinion)) {
+            $opinion = new OpinionModel;
+        }
+
+        \view\opinion\index($opinion, $topic, true);
+
+    }
+
+
+//    意見の登録処理
+    public function store(Request $request)
+    {
+        $opinion = new OpinionModel;
+
+        $opinion->opinion = get_param('opinion', null);
+        $opinion->reason = get_param('reason', null);
+        $opinion->topic_id = get_param('id', null, false);
+
+
+        try {
+            $validation = new OpinionValidation($opinion);
+
+            if (!$validation->checkCreate()) {
+                Msg::push(Msg::ERROR, '意見の登録に失敗しました。');
+
+                OpinionModel::setSession($opinion);
+
+                redirect(GO_REFERER);
+            }
+
+            $valid_data = $validation->getValidData();
+
+            OpinionQuery::insert($valid_data) ? Msg::push(Msg::INFO, '意見を登録しました。') : Msg::push(Msg::ERROR, '登録に失敗しました。');
+
+            redirect(sprintf('detail?id=%s', $opinion->topic_id));
+        } catch (Exception $e) {
+            Msg::push(Msg::ERROR, $e->getMessage());
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+//    意見の編集画面を表示
+    public function edit($id)
+    {
+        $topic = new TopicModel;
+
+        $topic->id = get_param('id', null, false);
+
+        // idからトピックの内容を取ってくる
+        $fetchedTopic = TopicQuery::fetchById($topic);
+
+        // トピックが取れてこなかったら４０４ページへリダイレクト
+        if (!$fetchedTopic) {
+            redirect('404');
+            return;
+        }
+
+        // バリデーションに引っかかって登録に失敗した場合の処理
+        // セッションに保存しておいた値を取ってきて変数に格納する。セッション上のデータは削除する
+        // 必ずデータを取得した時点でデータを削除しておく必要がある。そうしないと他の記事を選択したときに出てきてしまう。
+        $opinion = OpinionModel::getSessionAndFlush();
+
+        // データが取れてくれば、その値を画面表示し、処理を終了
+        if (!empty($opinion)) {
+            \view\opinion\index($opinion, $topic, false);
+            return;
+        }
+
+        // idからトピックの内容を取ってくる
+        $fetchedOpinion = OpinionQuery::fetchByTopicId($topic);
+
+        // トピックを渡してviewのindexを表示
+        \view\opinion\index($fetchedOpinion, $topic, false);
+
+    }
+
+
+//    意見の更新処理
+    public function update(Request $request, $id)
+    {
+        $opinion = new OpinionModel;
+
+        $opinion->id = get_param('id', null);
+        $opinion->opinion = get_param('opinion', null);
+        $opinion->reason = get_param('reason', null);
+        $opinion->topic_id = get_param('id', null, false);
+
+        try {
+            $validation = new OpinionValidation($opinion);
+
+            if (!$validation->checkEdit()) {
+                Msg::push(Msg::ERROR, '意見の更新に失敗しました。');
+                OpinionModel::setSession($opinion);
+                redirect(GO_REFERER);
+            }
+
+            $valid_data = $validation->getValidData();
+
+            OpinionQuery::update($valid_data) ? Msg::push(Msg::INFO, '意見を更新しました。') : Msg::push(Msg::ERROR, '更新に失敗しました。');
+
+            redirect(sprintf('detail?id=%s', $opinion->topic_id));
+        } catch (Exception $e) {
+            Msg::push(Msg::ERROR, $e->getMessage());
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
