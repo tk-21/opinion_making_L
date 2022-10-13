7b4361fc3b770c98b8b83273263bf5b21aa99b5a
tk <83660733+tk-21@users.noreply.github.com>
2022-10-14 07:22:39 +0900

    ルート追加

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+
+Route::get('/register', [\App\Http\Controllers\AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+
+Route::get('/detail/{topic}', [\App\Http\Controllers\TopicController::class, 'index'])->name('topics.index');
+Route::post('/detail/{topic}', [\App\Http\Controllers\ObjectionController::class, 'store'])->name('objection.store');
+
