41e1cc1e1604f95690d5c052dfee7bd8c4db84e7
tk <83660733+tk-21@users.noreply.github.com>
2022-10-15 12:54:03 +0900

    バリデーションルールの追加

--- app/Http/Requests/StoreTopicRequest.php
+++ app/Http/Requests/StoreTopicRequest.php
@@ -13,7 +13,7 @@ class StoreTopicRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class StoreTopicRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => ['required'],
+            'body' => ['required'],
+            'position' => ['required'],
         ];
     }
 }
--- app/Http/Requests/UpdateTopicRequest.php
+++ app/Http/Requests/UpdateTopicRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUserRequest extends FormRequest
+class UpdateTopicRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,9 +24,10 @@ class StoreUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'max:10'],
-            'password' => ['required', 'min:4', 'alpha-num'],
-            'email' => ['required', 'email:filter,dns']
+            'id' => ['required', 'exists:topics,id', 'numeric'],
+            'title' => ['required'],
+            'body' => ['required'],
+            'position' => ['required'],
         ];
     }
 }
