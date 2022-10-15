615c111e3158584cbcd5e9abb9dd06f05957da48
tk <83660733+tk-21@users.noreply.github.com>
2022-10-16 07:23:48 +0900

    ユーザーテーブルのマイグレーション作成

--- database/migrations/2022_10_15_214523_create_users_table.php
+++ database/migrations/2022_10_15_214523_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id()->comment('ユーザーID');
+            $table->string('name', 10)->comment('ユーザーネーム');
+            $table->string('password', 60)->comment('パスワード');
+            $table->string('email', 255)->unique()->comment('メールアドレス');
+            $table->timestamp('created_at')->useCurrent()->nullable(false)->comment('作成日時');
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate()->nullable(false)->comment('更新日時');
+            $table->timestamp('deleted_at')->nullable()->comment('削除日時');
         });
     }
 
