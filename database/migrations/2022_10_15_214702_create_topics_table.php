79d7408ba8109f9279105a6d3d9757dd7d575d30
tk <83660733+tk-21@users.noreply.github.com>
2022-10-16 22:19:02 +0900

    トピックスのマイグレーションファイルを編集

--- database/migrations/2022_10_15_214702_create_topics_table.php
+++ database/migrations/2022_10_15_214702_create_topics_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('topics', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id()->comment('トピックID');
+            $table->text('title')->comment('トピックタイトル');
+            $table->text('body')->comment('トピック本文');
+            $table->text('position')->comment('トピックに対して自らがとるポジション');
+            $table->integer('status')->default('0')->comment('完了フラグ（1:完了　0:未完了）');
+            $table->integer('category_id')->comment('カテゴリーID');
+            $table->string('user_id', 10)->comment('作成したユーザーID');
+            $table->timestamp('created_at')->useCurrent()->nullable(false)->comment('作成日時');
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate()->nullable(false)->comment('更新日時');
+            $table->timestamp('deleted_at')->nullable()->comment('削除日時');
         });
     }
 
