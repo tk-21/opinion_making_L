8f8d872cc9bf2e6424ca8cfed4c71008861f802e
tk <83660733+tk-21@users.noreply.github.com>
2022-10-17 22:45:18 +0900

    ユーザー登録処理、ログイン処理を追加

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Login\LoginUserRequest;
+use App\Http\Requests\Store\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -14,51 +19,81 @@ class AuthController extends Controller
     }
 
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
+        $credentials = $request->validated();
+
+        // Auth::attemptメソッドでログイン情報が正しいか検証
+        if (Auth::attempt($credentials)) {
+            // セッションを再生成する処理(セキュリティ対策)
+            $request->session()->regenerate();
+
+            // ミドルウェアに対応したリダイレクト(後述)
+            // 下記はredirect('/admin/blogs')に類似
+            return redirect()->intended('/admin/blogs');
+
+            // 一つ前のページ(ログイン画面)にリダイレクト
+            // その際にwithErrorsを使ってエラーメッセージで手動で指定する
+            // リダイレクト後のビュー内でold関数によって直前の入力内容を取得出来る項目をonlyInputで指定する
+            return back()->withErrors([
+                'email' => 'メールアドレスまたはパスワードが正しくありません',
+            ])->onlyInput('email');
+        }
+
+
         // 値の取得
-        $name = get_param('name', '');
-        $password = get_param('password', '');
-
-        // バリデーション
-        $validation = new UserValidation($name, $password);
-
-        if (
-            !($validation->validateName()
-                * $validation->validatePassword())
-        ) {
-            redirect(GO_REFERER);
-        }
-
-        $valid_name = $validation->getValidName();
-        $valid_password = $validation->getValidPassword();
-
-        // POSTで渡ってきたユーザーネームとパスワードでログインに成功した場合、
-        if (Auth::login($valid_name, $valid_password)) {
-            // 登録されたユーザーオブジェクトの情報を取ってくる
-            $user = UserModel::getSession();
-            // オブジェクトに格納されている情報を使って、セッションのINFOにメッセージを入れる
-            Msg::push(Msg::INFO, "{$user->name}さん、ようこそ。");
-            // パスが空だったらトップページに移動
-            redirect(GO_HOME);
-        } else {
-            // Auth::loginによって何がエラーかというのはpushされるので、ここでエラーメッセージは出さなくてよい
-
-            // refererは一つ前のリクエストのパスを表す
-            // 認証が失敗したときは、一つ前のリクエスト（GETメソッドでのログインページへのパス）に戻る
-            redirect(GO_REFERER);
-        }
+//        $name = get_param('name', '');
+//        $password = get_param('password', '');
+//
+//        // バリデーション
+//        $validation = new UserValidation($name, $password);
+//
+//        if (
+//            !($validation->validateName()
+//                * $validation->validatePassword())
+//        ) {
+//            redirect(GO_REFERER);
+//        }
+//
+//        $valid_name = $validation->getValidName();
+//        $valid_password = $validation->getValidPassword();
+//
+//        // POSTで渡ってきたユーザーネームとパスワードでログインに成功した場合、
+//        if (Auth::login($valid_name, $valid_password)) {
+//            // 登録されたユーザーオブジェクトの情報を取ってくる
+//            $user = UserModel::getSession();
+//            // オブジェクトに格納されている情報を使って、セッションのINFOにメッセージを入れる
+//            Msg::push(Msg::INFO, "{$user->name}さん、ようこそ。");
+//            // パスが空だったらトップページに移動
+//            redirect(GO_HOME);
+//        } else {
+//            // Auth::loginによって何がエラーかというのはpushされるので、ここでエラーメッセージは出さなくてよい
+//
+//            // refererは一つ前のリクエストのパスを表す
+//            // 認証が失敗したときは、一つ前のリクエスト（GETメソッドでのログインページへのパス）に戻る
+//            redirect(GO_REFERER);
+//        }
     }
 
 
     public function logout()
     {
-        if (Auth::logout()) {
-            Msg::push(Msg::INFO, 'ログアウトしました。');
-            redirect('login');
-        } else {
-            Msg::push(Msg::ERROR, 'ログアウトに失敗しました。');
-        }
+//        if (Auth::logout()) {
+//            Msg::push(Msg::INFO, 'ログアウトしました。');
+//            redirect('login');
+//        } else {
+//            Msg::push(Msg::ERROR, 'ログアウトに失敗しました。');
+//        }
+
+        // ログアウト処理
+        Auth::logout();
+        // 現在使っているセッションを無効化(セキュリティ対策のため)
+        $request->session()->invalidate();
+        // セッションを無効化を再生成(セキュリティ対策のため)
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
+
     }
 
 
@@ -73,35 +108,23 @@ class AuthController extends Controller
     }
 
 
-    public function register()
+    public function register(StoreUserRequest $request)
     {
-        // 値の取得
-        $name = get_param('name', '');
-        $password = get_param('password', '');
-        $email = get_param('email', '');
+        $validated = $request->validated();
 
-        // バリデーション
-        $validation = new UserValidation($name, $password, $email);
+        $validated['password'] = Hash::make($validated['password']);
 
-        if (
-            !($validation->validateName()
-                * $validation->validatePassword()
-                * $validation->validateEmail())
-        ) {
-            redirect(GO_REFERER);
-        }
+        User::create($validated);
 
-        $valid_name = $validation->getValidName();
-        $valid_password = $validation->getValidPassword();
-        $valid_email = $validation->getValidEmail();
+        return to_route('login')->with('success', '登録が完了しました');
 
         // 登録処理
-        if (Auth::regist($valid_name, $valid_password, $valid_email)) {
-            Msg::push(Msg::INFO, "{$name}さん、ようこそ。");
-            redirect(GO_HOME);
-        } else {
-            redirect(GO_REFERER);
-        }
+//        if (Auth::regist($valid_name, $valid_password, $valid_email)) {
+//            Msg::push(Msg::INFO, "{$name}さん、ようこそ。");
+//            redirect(GO_HOME);
+//        } else {
+//            redirect(GO_REFERER);
+//        }
     }
 
 }
