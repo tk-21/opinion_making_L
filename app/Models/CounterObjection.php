fbf6ea0df46778ef0cd2d57ad50dacafe43dd176
tk <83660733+tk-21@users.noreply.github.com>
2022-10-23 07:40:21 +0900

    各モデルにを設定

--- app/Models/CounterObjection.php
+++ app/Models/CounterObjection.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CounterObjection extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'topic_id'
+    ];
 }
--- app/Models/Opinion.php
+++ app/Models/Opinion.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Opinion extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'opinion',
+        'reason',
+        'topic_id'
+    ];
 }
