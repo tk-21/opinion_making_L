85808ec75bb3e12132c9b2019ddebc1962da5bce
tk <83660733+tk-21@users.noreply.github.com>
2022-10-25 07:15:23 +0900

    一覧画面のトピック存在確認、詳細画面と削除確認画面のカテゴリー表示を修正

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -2,12 +2,11 @@
 @section('content')
 
     @php
-        dd($topic);
-            $status = $topic->status ? '完了' : '未完了';
+        $status = $topic->status ? '完了' : '未完了';
 
-            // カテゴリーが削除されていれば未選択にする
-            $category_name = $topic->category->deleted_at ? '未選択' : $topic->category->name;
-    //        $is_edit = $opinion ? 'edit' : 'create';
+        // カテゴリーが削除されていれば未選択にする
+        $category_name = empty($topic->category->deleted_at) ? '未選択' : $topic->category->name;
+//        $is_edit = $opinion ? 'edit' : 'create';
     @endphp
 
     <section class="detail">
