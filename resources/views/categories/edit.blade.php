339004c8f7629743663f1c4ef6ba5ec3e9c48cf3
tk <83660733+tk-21@users.noreply.github.com>
2022-10-27 12:50:22 +0900

    カテゴリー編集画面を表示できるよう修正

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,20 +1,19 @@
 @extends('layouts.default')
 @section('content')
-    {{--    $category = escape($category);--}}
-
-
 
     <section class="edit">
         <div class="edit-inner">
-            <form class="edit-form validate-form" action="" method="POST" novalidate>
-                <input type="hidden" name="id" value="<?php echo $category->id; ?>">
+            <form class="edit-form validate-form" action="{{ route('categories.update',['category' => $category]) }}"
+                  method="POST" novalidate>
+                @csrf
+                @method('PUT')
 
                 <dl class="edit-list">
 
                     <dt class="edit-dttl"><label for="name" onclick="">カテゴリー名の編集</label></dt>
                     <dd class="edit-item">
                         <textarea id="name" name="name" class="edit-name input validate-target" autofocus
-                                  required><?php echo $category->name; ?></textarea>
+                                  required>{{ old('name', $category->name) }}</textarea>
                         <p class="invalid-feedback"></p>
                     </dd>
 
@@ -22,7 +21,7 @@
 
                 <button type="submit" class="register-btn">更新</button>
 
-                <a class="back-btn _back" href="<?php the_url(sprintf('category?id=%d', $category->id)); ?>">戻る</a>
+                <a class="back-btn _back" href="{{ route('categories.show',['category' => $category]) }}">戻る</a>
 
             </form>
         </div>
