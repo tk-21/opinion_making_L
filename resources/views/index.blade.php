a62939511558a0930a6b86c2cc4772798995e005
tk <83660733+tk-21@users.noreply.github.com>
2022-10-30 00:56:52 +0900

    ページャーが正しく表示されるように調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,42 +47,46 @@
                             @endforeach
                         </ul>
 
-                        <div class="paging">
-                            <p class="paging-txt">全件数：{{ $topics->total() }}件</p>
-                            <p class="paging-txt">{{ $topics->firstItem() }}~{{ $topics->lastItem() }}を表示中</p>
-                            <ul class="paging-list">
+                        @if($topics->hasPages())
+                            <div class="paging">
+                                <p class="paging-txt">全件数：{{ $topics->total() }}件</p>
+                                <p class="paging-txt">{{ $topics->firstItem() }}~{{ $topics->lastItem() }}を表示中</p>
+                                <ul class="paging-list">
 
-                                {{--                                前へ戻るボタン--}}
-                                <li class="paging-item">
-                                    @if ($topics->onFirstPage())
-                                        <span class="paging-pre">&laquo;</span>
-                                    @else
-                                        <a href="{{ $topics->previousPageUrl() }}">&laquo;</a>
-                                    @endif
-                                </li>
-
-                                {{--                                ページ番号--}}
-                                @foreach($topics->getUrlRange($topics->currentPage()-3, $topics->currentPage()+3) as $page=>$link)
+                                    {{--                                前へ戻るボタン--}}
                                     <li class="paging-item">
-                                        @if($page === $topics->currentPage())
-                                            <span class="paging-now">{{ $page }}</span>
+                                        @if ($topics->onFirstPage())
+                                            <span class="paging-pre">&laquo;</span>
                                         @else
-                                            <a href="{{ $link }}" class="paging-num">{{ $page }}</a>
+                                            <a href="{{ $topics->previousPageUrl() }}">&laquo;</a>
                                         @endif
                                     </li>
-                                @endforeach
 
-                                {{--                                次へ進むボタン--}}
-                                <li class="paging-item">
-                                    @if($topics->hasMorePages())
-                                        <a href="{{ $topics->nextPageUrl() }}">&raquo;</a>
-                                    @else
-                                        <span class="paging-next">&raquo;</span>
-                                    @endif
-                                </li>
+                                    {{--                                ページ番号--}}
+                                    @foreach($topics->getUrlRange($topics->currentPage()-3, $topics->currentPage()+3) as $page=>$link)
+                                        <li class="paging-item">
+                                            @if($page >= 1 && $page <= $topics->lastPage())
+                                                @if($page === $topics->currentPage())
+                                                    <span class="paging-now">{{ $page }}</span>
+                                                @else
+                                                    <a href="{{ $link }}" class="paging-num">{{ $page }}</a>
+                                                @endif
+                                            @endif
+                                        </li>
+                                    @endforeach
 
-                            </ul>
-                        </div>
+                                    {{--                                次へ進むボタン--}}
+                                    <li class="paging-item">
+                                        @if($topics->hasMorePages())
+                                            <a href="{{ $topics->nextPageUrl() }}">&raquo;</a>
+                                        @else
+                                            <span class="paging-next">&raquo;</span>
+                                        @endif
+                                    </li>
+
+                                </ul>
+                            </div>
+                        @endif
 
                     @else
 
