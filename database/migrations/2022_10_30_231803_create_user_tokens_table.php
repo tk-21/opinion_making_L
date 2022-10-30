7e3d342d549bea1f267fc06de135aa224a790a81
tk <83660733+tk-21@users.noreply.github.com>
2022-10-30 23:34:51 +0900

    マイグレーションファイルの修正

---
+++ database/migrations/2022_10_30_231803_create_user_tokens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->comment('ユーザーID');
+            $table->string('token')->unique()->comment('トークン');
+            $table->dateTime('expire_at')->nullable()->comment('トークンの有効期限');
+            $table->timestamps();
+            $table->comment('ユーザートークン');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_tokens');
+    }
+};
